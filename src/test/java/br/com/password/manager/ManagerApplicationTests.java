7319c1cc51fbfaaa44d230208f08ef8da3a6bb4c
rafaelmadakis <rafaelmadakis@gmail.com>
2020-08-21 20:19:06 -0300

    criação inicial

---
+++ src/test/java/br/com/password/manager/ManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.password.manager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ManagerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
