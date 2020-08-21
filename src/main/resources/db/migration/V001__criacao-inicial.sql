6570102cb3bba3287b5de9dd9c8bf43b4ec45919
rafaelmadakis <rafaelmadakis@gmail.com>
2020-08-21 20:24:01 -0300

    Migração tabela dados

--- src/main/resources/db/migration/V001__criacao-inicial.sql
+++ src/main/resources/db/migration/V001__criacao-inicial.sql
@@ -1,8 +1,8 @@
 create table dados (
     id bigint not null auto_increment,
-    sistema vachar(60) not null,
+    sistema varchar(60) not null,
     usuario varchar(15) not null,
     senha varchar(20) not null,
 
     primary key (id)
-) engine=InnoDB default character=utf8;
+) engine=InnoDB default charset=utf8;
