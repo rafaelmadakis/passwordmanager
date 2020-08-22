d38bfd29bb59da6cef76cd9100adb67f0ce4b46f
rafaelmadakis <rafaelmadakis@gmail.com>
2020-08-21 23:19:34 -0300

    Correção nome do Disassembler

--- src/main/java/br/com/password/manager/api/assembler/DadoInputDisassembler.java
+++ src/main/java/br/com/password/manager/api/assembler/DadoInputDisassembler.java
@@ -8,7 +8,7 @@ import org.springframework.stereotype.Component;
 
 
 @Component
-public class CidadeInputDisassembler {
+public class DadoInputDisassembler {
 
     @Autowired
     private ModelMapper modelMapper;
