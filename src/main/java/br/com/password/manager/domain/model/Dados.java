ab53f279a30a357be751f8881865276826d97114
rafaelmadakis <rafaelmadakis@gmail.com>
2020-08-21 21:11:08 -0300

    Criação do model

---
+++ src/main/java/br/com/password/manager/domain/model/Dados.java
@@ -0,0 +1,30 @@
+package br.com.password.manager.domain.model;
+
+import com.sun.istack.NotNull;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Data
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@Entity
+public class Dados {
+
+    @EqualsAndHashCode.Include
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotNull
+    private String sistema;
+
+    @NotNull
+    private String usuario;
+
+    @NotNull
+    private String senha;
+}
