2a7dc5c08242f8b908fcbbee35eab1ade9b7249a
rafaelmadakis <rafaelmadakis@gmail.com>
2020-08-21 23:11:44 -0300

    Criação do Disassembler

---
+++ src/main/java/br/com/password/manager/api/assembler/CidadeInputDisassembler.java
@@ -0,0 +1,29 @@
+package br.com.password.manager.api.assembler;
+
+import br.com.password.manager.api.model.input.DadoInput;
+import br.com.password.manager.domain.model.Dado;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+
+@Component
+public class CidadeInputDisassembler {
+
+    @Autowired
+    private ModelMapper modelMapper;
+
+    public Dado toDomainObject(DadoInput dadoInput) {
+        return modelMapper.map(dadoInput, Dado.class);
+    }
+
+    public void copyToDomainObject(DadoInput dadoInput, Dado dado) {
+
+        // Para evitar org.hibernate.HibernateException: identifier of an instance of
+        // com.algaworks.algafood.domain.model.Estado was altered from 1 to 2
+//        cidade.setEstado(new Estado());
+
+        modelMapper.map(dadoInput, dado);
+
+    }
+}
