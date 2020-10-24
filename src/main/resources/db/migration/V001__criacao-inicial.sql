8b781fb0e3016938b1e467005de0e4709bc84b9d
rafaelmadakis <rafaelmadakis@gmail.com>
2020-10-24 17:58:28 -0300

    Alteração de perfil de produção e alteração do SQL na migração

--- src/main/resources/db/migration/V001__criacao-inicial.sql
+++ src/main/resources/db/migration/V001__criacao-inicial.sql
@@ -1,8 +1,8 @@
 create table dado (
      id bigint not null auto_increment,
     sistema varchar(60) not null,
-    usuario varchar(15) not null,
-    senha varchar(20) not null,
+    usuario varchar(50) not null,
+    senha varchar(60) not null,
 
     primary key (id)
 ) engine=InnoDB default charset=utf8;
